0ed1fea174801d179271f0c7e244797693abd2c1
sedat.akkus <sedat@base86.net>
2023-04-27 22:52:03 +0300

    feat: add divide operation and minor refactor

--- Program.cs
+++ Program.cs
@@ -15,21 +15,17 @@
             string operationType = Console.ReadLine().ToLower();
 
             
-            if (operationType == "topla" || operationType == "+" || operationType == "toplama")
-            {
+            if (operationType == "+" || operationType == "topla" || operationType == "toplama")
                 Console.WriteLine("{0} + {1} = {2}", firstNumber, secondNumber, (firstNumber + secondNumber));
-            }
             
             else if (operationType == "-" || operationType == "çıkar" || operationType == "çıkarma")
-            {
                 Console.WriteLine("{0} - {1} = {2}", firstNumber, secondNumber, (firstNumber - secondNumber));
-                
-            }
 
             else if (operationType == "*" || operationType == "çarp" || operationType == "çarpma")
-            {
                 Console.WriteLine("{0} x {1} = {2}" , firstNumber, secondNumber, (firstNumber * secondNumber));
-            }
+
+            else if (operationType == "/" || operationType == "böl" || operationType == "bölme")
+                Console.WriteLine("{0} / {1} = {2}", firstNumber, secondNumber, (firstNumber / secondNumber));
 
 
         }
